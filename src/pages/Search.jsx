bb4d8f04542e6ae99aca29f8ae257f2098e24e8b
wndud <ju03209@naver.com>
2025-06-11 13:48:00 +0900

    feat: debounce를 활용한 검색 자동완성 추가

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,22 @@
 import { Link, useSearchParams } from "react-router-dom";
 import { data } from "../assets/data/data";
 import { getRegExp } from "korean-regexp";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const [searchParams] = useSearchParams();
+  const [filtereData, setFilterData] = useState(data);
   const param = searchParams.get("animal");
   const reg = getRegExp(param);
 
-  const filtereData = data.filter((el) => el.name.match(reg));
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      const newFilterData = data.filter((el) => el.name.match(reg));
+      setFilterData(newFilterData);
+    }, 100);
+    return () => clearTimeout(debounceTimer);
+  }, [param]);
+
   return (
     <ul>
       {filtereData.map((el) => (
